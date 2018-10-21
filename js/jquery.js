a8f661fc5e9c688ba3d766d6d621d60f421a8bbe
Tiffany <tiffanyjewelsims@gmail.com>
2018-10-20 19:14:56 -0500

    deploy

---
+++ js/jquery.js
@@ -0,0 +1,36 @@
+$('.js-scroll').click(function(){
+    $('html,body').animate({scrollTop:$('.js-cards').offset().top},1000);
+    $(".navigation__checkbox").prop('checked',false);
+
+   
+
+
+});
+
+$('.js-scroll-about').click(function(){
+    $('html,body').animate({scrollTop:$('.js-about').offset().top},1000);
+
+    $(".navigation__checkbox").prop('checked',false);
+
+
+   
+
+});
+
+$('.js-scroll-benefits').click(function(){
+    $('html,body').animate({scrollTop:$('.js-benefits').offset().top},1000);
+    $(".navigation__checkbox").prop('checked',false);
+
+});
+
+$('.js-scroll-test').click(function(){
+    $('html,body').animate({scrollTop:$('.js-test').offset().top},1000);
+    $(".navigation__checkbox").prop('checked',false);
+
+});
+
+$('.js-scroll-book').click(function(){
+    $('html,body').animate({scrollTop:$('.js-book').offset().top},1000);
+    $(".navigation__checkbox").prop('checked',false);
+
+});
